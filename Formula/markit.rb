2cab2ceef586db50df18dd41c2fb2cd7fd70091e
Ryan Palmer <rpalmer9696@gmail.com>
2025-06-18 16:24:54 +0100

    Bump markit to v1.1.8

--- Formula/markit.rb
+++ Formula/markit.rb
@@ -1,8 +1,8 @@
 class Markit < Formula
   desc "A blazing-fast CLI to save, search, run, and manage your shell command snippets"
   homepage "https://github.com/Nightstack/markit"
-  url "https://github.com/Nightstack/markit/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "dfdf8529c86e7b79fa49dc898cc7085daf5991db30a396a7b7c897241a6eba97"
+  url "https://github.com/Nightstack/markit/archive/refs/tags/v1.1.8.tar.gz"
+  sha256 "d076d502d1effbbf507b613f8aa89c722e694d8bd0fc0f8251b1b79e20086d4d"
   license "MIT"
 
   depends_on "rust" => :build
